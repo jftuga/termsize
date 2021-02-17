85039a92f2b6ca5af9b93aad6b9824abee6617fe
John Taylor <johntaylor@gmail.com>
2021-02-17 00:56:51 -0500

    add -v to get version info

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,33 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/jftuga/termsize"
 )
 
+var (
+	Version string = strings.TrimSpace(version)
+	//go:embed version.txt
+	version string
+)
+
+func info() {
+	fmt.Printf("terminfo v%s\n", Version)
+	fmt.Println("https://github.com/jftuga/termsize")
+	fmt.Println()
+}
+
 func main() {
-	termsize.DefaultHeight = 133
+	if len(os.Args) == 2 && (os.Args[1] == "-h" || os.Args[1] == "-?" || os.Args[1] == "-v") {
+		info()
+		return
+	}
+
+	termsize.DefaultHeight = 120
 	w := termsize.Width()
 	h := termsize.Height()
 	fmt.Printf("%dx%d\n", w, h)
